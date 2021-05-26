9134c3ce6dbcc11096456240516870797824d7fc
charitha <charitha.m@sonata-software.com>
2021-05-26 22:02:24 +0530

    Simple spring core project is added

---
+++ src/main/java/com/sonata/HelloService.java
@@ -0,0 +1,15 @@
+package com.sonata;
+
+public class HelloService {
+	private String name;
+	
+	public void setName(String name) {
+		this.name=name;
+		
+	}
+	
+	public String sayHello() {
+		return "hello!" +name;
+	}
+
+}
